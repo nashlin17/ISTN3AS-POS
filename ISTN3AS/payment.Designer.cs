48f7bfc6fa72ec81155343b0771b94ba93fba4f5
Ashlin-Gov <Ashlin.G@hotmail.com>
2020-08-25 08:25:30 +0200

    Final

--- ISTN3AS/payment.Designer.cs
+++ ISTN3AS/payment.Designer.cs
@@ -249,7 +249,6 @@
             this.button3.TabIndex = 6;
             this.button3.Text = "Pay With Card";
             this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // panel8
             // 
